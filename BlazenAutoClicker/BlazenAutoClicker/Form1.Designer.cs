d2e53393d18f32ecabb19fc95eb6e3236f6724f1
Camyul <vankata.lt@outlook.com>
2018-07-13 11:43:50 +0300

    Added Start Mining and Refresh buttons

---
+++ BlazenAutoClicker/BlazenAutoClicker/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace BlazenAutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pContainer = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pContainer
+            // 
+            this.pContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pContainer.Location = new System.Drawing.Point(12, 12);
+            this.pContainer.Name = "pContainer";
+            this.pContainer.Size = new System.Drawing.Size(621, 530);
+            this.pContainer.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(639, 41);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(122, 66);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Mining";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_refresh.Location = new System.Drawing.Point(639, 129);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(122, 43);
+            this.btn_refresh.TabIndex = 2;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 554);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pContainer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "BlazonAutoClicker";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pContainer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btn_refresh;
+    }
+}
+
---
+++ BlazenAutoClicker/BlazenAutoClicker/Form1.cs
@@ -0,0 +1,99 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BlazenAutoClicker
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public bool startMining;
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeChromium();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+
+            Cef.Initialize(settings);
+            chromeBrowser = new ChromiumWebBrowser("http://www.blazoncommunity.com/Webwork/User/UserPanel.php?Login=Yes");
+            //chromeBrowser = new ChromiumWebBrowser("https://www.google.bg/");
+
+            this.pContainer.Controls.Add(chromeBrowser);
+
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            chromeBrowser.FrameLoadStart += (o, e) => chromeBrowser.SetZoomLevel(-2.8);
+
+            //Wait for the page to finish loading (all resources will have been loaded, rendering is likely still happening)
+            //chromeBrowser.LoadingStateChanged += (sender, args) =>
+            //{
+            //    //Wait for the Page to finish loading
+            //    if (args.IsLoading == false)
+            //    {
+            //        chromeBrowser.ExecuteScriptAsync("alert('All Resources Have Loaded');");
+            //    }
+            //};
+
+            //Wait for the MainFrame to finish loading
+            chromeBrowser.FrameLoadEnd += (sender, args) =>
+            {
+                //Wait for the MainFrame to finish loading
+                if (this.startMining && args.Frame.IsMain)
+                {
+                    string script = getJavaScript();
+                    args.Frame.ExecuteJavaScriptAsync(script);
+                }
+            };
+        }
+
+        private string getJavaScript()
+        {
+            string path = string.Format(@"{0}\HTMLResources\js\script-no-bonus.js", Application.StartupPath);
+            //string path = string.Format(@"{0}\HTMLResources\js\script-bonus.js", Application.StartupPath);
+            string script = File.ReadAllText(path);
+
+            //chromeBrowser.ShowDevTools();
+            return script;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!this.startMining)
+            {
+                this.btnStart.Text = "Stop Mining";
+                this.startMining = true;
+
+                string script = getJavaScript();
+                chromeBrowser.ExecuteScriptAsync(script);
+            }
+            else
+            {
+                this.btnStart.Text = "Start Mining";
+                this.startMining = false;
+            }
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            chromeBrowser.Reload();
+        }
+    }
+}
